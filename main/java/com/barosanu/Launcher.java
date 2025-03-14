53c195f99153146e38c1125aaa84c2c939e8e046
xXMarekBXx <57196180+xXMarekBXx@users.noreply.github.com>
2025-03-14 18:13:47 +0100

    Add two application windows
    Create two windows using "SceneBuilder" application and code the basics, implementing them using the factory design pattern.
    Completed the first two sections of the course: "Advanced Java programming with JavaFx: Write an email client" form udemy.com

---
+++ main/java/com/barosanu/Launcher.java
@@ -0,0 +1,29 @@
+package com.barosanu;
+
+import com.barosanu.view.ViewFactory;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Launcher extends Application{
+
+    @FXML
+    public void buttonAction(){
+        System.out.println("Clicked on Button!");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+
+        ViewFactory viewFactory = new ViewFactory(new EmailManager());
+        viewFactory.showLoginWindow();
+    }
+}
---
+++ main/java/com/barosanu/controller/LoginWindowController.java
@@ -0,0 +1,29 @@
+package com.barosanu.controller;
+
+import com.barosanu.EmailManager;
+import com.barosanu.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController extends BaseController {
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("Click!");
+    }
+}
