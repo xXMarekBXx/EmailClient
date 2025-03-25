0d9c8ddecc076d74d092785e49e62df7391fdf10
xXMarekBXx <57196180+xXMarekBXx@users.noreply.github.com>
2025-03-25 21:14:31 +0100

    Add emails handling
    Add emails handling.
    End 6th section of course from udemy.com

--- main/java/com/barosanu/controller/MainWindowController.java
+++ main/java/com/barosanu/controller/MainWindowController.java
@@ -1,14 +1,23 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.services.MessageRendererService;
+import com.barosanu.model.EmailMessage;
+import com.barosanu.model.EmailTreeItem;
+import com.barosanu.model.SizeInteger;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.web.WebView;
+import javafx.util.Callback;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class MainWindowController extends BaseController implements Initializable {
@@ -17,10 +26,31 @@ public class MainWindowController extends BaseController implements Initializabl
     private TreeView<String> emailsTreeView;
 
     @FXML
-    private TableView<?> emailsTableView;
+    private TableView<EmailMessage> emailsTableView;
+
+    //
+
+    @FXML
+    private TableColumn<EmailMessage, String> senderCol;
+
+    @FXML
+    private TableColumn<EmailMessage, String> subjectCol;
+
+    @FXML
+    private TableColumn<EmailMessage, String> recipientCol;
+
+    @FXML
+    private TableColumn<EmailMessage, SizeInteger> sizeCol;
+
+    @FXML
+    private TableColumn<EmailMessage, Date> dateCol;
+
+    //
 
     @FXML
     private WebView emailWebView;
+
+    private MessageRendererService messageRendererService;
 
     public MainWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
@@ -39,8 +69,64 @@ public class MainWindowController extends BaseController implements Initializabl
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         setUpEmailsTreeView();
+        setUpEmailsTableView();
+        setUpFolderSelection();
+        setUpBoldRows();
+        setUpMessageRendererService();
+        setUpMessageSelection();
+    }
+
+    private void setUpMessageSelection() {
+        emailsTableView.setOnMouseClicked(event->{
+            EmailMessage emailMessage = emailsTableView.getSelectionModel().getSelectedItem();
+            if(emailMessage != null){
+                messageRendererService.setEmailMessage(emailMessage);
+                messageRendererService.restart();
+            }
+        });
+    }
+
+    private void setUpMessageRendererService() {
+        messageRendererService = new MessageRendererService(emailWebView.getEngine());
+    }
+
+    private void setUpBoldRows() {
+        emailsTableView.setRowFactory(new Callback<TableView<EmailMessage>, TableRow<EmailMessage>>() {
+            @Override
+            public TableRow<EmailMessage> call(TableView<EmailMessage> param) {
+                return new TableRow<EmailMessage>() {
+                    @Override
+                    protected void updateItem(EmailMessage item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item != null) {
+                            if (item.isRead()) {
+                                setStyle("");
+                            } else {
+                                setStyle("-fx-font-weight: bold;");
+                            }
+                        }
+                    }
+                };
+            }
+        });
+    }
+
+    private void setUpFolderSelection() {
+        emailsTreeView.setOnMouseClicked(e->{
+            EmailTreeItem<String> item = (EmailTreeItem<String>)emailsTreeView.getSelectionModel().getSelectedItem();
+            if(item != null){
+                emailsTableView.setItems(item.getEmailMessages());
+            }
+        });
+    }
+
+    private void setUpEmailsTableView() {
+        senderCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("sender"));
+        subjectCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("subject"));
+        recipientCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("recipient"));
+        sizeCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, SizeInteger>("size"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Date>("date"));
     }
 
     private void setUpEmailsTreeView() {
--- main/java/com/barosanu/controller/services/FetchFolderService.java
+++ main/java/com/barosanu/controller/services/FetchFolderService.java
@@ -31,18 +31,41 @@ public class FetchFolderService extends Service<Void> {
 
     private void fetchFolders() throws MessagingException {
         Folder[] folders = store.getDefaultFolder().list();
-        handlerFolders(folders, foldersRoot);
+        handleFolders(folders, foldersRoot);
     }
 
-    private void handlerFolders(Folder[] folders, EmailTreeItem<String> foldersRoot) throws MessagingException {
+    private void handleFolders(Folder[] folders, EmailTreeItem<String> foldersRoot) throws MessagingException {
         for(Folder folder: folders){
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
             foldersRoot.getChildren().add(emailTreeItem);
             foldersRoot.setExpanded(true);
+            fetchMessagesOnFolder(folder, emailTreeItem);
             if(folder.getType() == Folder.HOLDS_FOLDERS){
                 Folder[] subFolders = folder.list();
-                handlerFolders(subFolders, emailTreeItem);
+                handleFolders(subFolders, emailTreeItem);
             }
         }
+    }
+
+    private void fetchMessagesOnFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
+        Service fetchMessagesService = new Service() {
+            @Override
+            protected Task createTask() {
+                return new Task() {
+                    @Override
+                    protected Object call() throws Exception {
+                        if(folder.getType() != Folder.HOLDS_FOLDERS){
+                            folder.open(Folder.READ_WRITE);
+                            int folderSize = folder.getMessageCount();
+                            for(int i = folderSize; i > 0; i--){
+                                emailTreeItem.addEmail(folder.getMessage(i));
+                            }
+                        }
+                        return null;
+                    }
+                };
+            }
+        };
+        fetchMessagesService.start();
     }
 }
